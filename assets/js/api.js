169dda78c789f63f916db12b76e9e8ea85148135
antdol <antoine.dolivet@gmail.com>
2024-01-05 14:35:03 +0100

    changement de façcon de faire pour le select

--- assets/js/api.js
+++ assets/js/api.js
@@ -10,73 +10,96 @@ async function getAllCharacters(
 ) {
     try {
         const response = await fetch(url)
+        if (!response.ok) {
+            throw new Error(response.status)
+        }
         const characters = await response.json()
-        const results = characters.results
 
-        createNav(
-            characters.info.prev,
-            characters.info.next,
-            characters.info.pages
-        )
+        // createNav(
+        //     characters.info.prev,
+        //     characters.info.next,
+        //     characters.info.pages
+        // )
+        const prev = characters.info.prev
+        const next = characters.info.next
+        const pages = characters.info.pages
+        nav.innerHTML = ''
+        if (prev) {
+            nav.innerHTML += `<button type="button" id="prev-btn" class="absolute left-5" onclick="navBtnHandleClick('${prev}', ${
+                currentPage - 1
+            })"><</button>`
+        }
+        nav.innerHTML += `
+        <select name="select-page" id="select-page" onchange="handleClick()">
+            <option value="">--Aller à--</option>`
+        const selectPage = document.querySelector('#select-page')
+        for (let i = 1; i < pages + 1; i++) {
+            selectPage.innerHTML += `
+            <option value="${i}">${i}</option>`
+        }
+        pageNumber.innerHTML = `${currentPage}/${pages}`
+        nav.append(pageNumber)
+        if (next) {
+            nav.innerHTML += `<button type="button" id="next-btn" class="absolute right-5" onclick="navBtnHandleClick('${next}', ${
+                currentPage + 1
+            })">></button>`
+        }
 
         // Affiche les personnage sous forme grille
-        container.innerHTML = ''
-        results.forEach((result) => {
-            const card = document.createElement('div')
-            card.classList.add('card')
-            card.innerHTML = `
-        <img src="${result.image}" alt="${result.name}" />
-        <h2>${result.name}</h2>`
-            container.appendChild(card)
-        })
-    } catch (error) {}
-}
-
-function createNav(prev, next, pages) {
-    nav.innerHTML = ''
-    if (prev) {
-        nav.innerHTML += `<button type="button" id="prev-btn" class="absolute left-5" onclick="navBtnHandleClick('${prev}', ${
-            currentPage - 1
-        })"><</button>`
-    }
-    const pageSelect = createSelectElement(pages)
-    // pageSelect.onchange = pageSelectHandleChange
-    nav.append(pageSelect)
-    pageNumber.innerHTML = `${currentPage}/${pages}`
-    nav.append(pageNumber)
-    if (next) {
-        nav.innerHTML += `<button type="button" id="next-btn" class="absolute right-5" onclick="navBtnHandleClick('${next}', ${
-            currentPage + 1
-        })">></button>`
+        displayCharacters(characters.results)
+    } catch (error) {
+        console.log(error)
     }
 }
 
-function createSelectElement(pages) {
-    const pageSelect = document.createElement('select')
-    pageSelect.name = 'page-select'
-    pageSelect.id = 'page-select'
-    pageSelect.innerHTML = `
-    <option value="">--Aller à--</option>`
-    for (let i = 1; i < pages + 1; i++) {
-        const option = document.createElement('option')
-        option.value = i
-        option.innerText = `page ${i}`
-        pageSelect.append(option)
-    }
-    pageSelect.onchange = pageSelectHandleChange()
-
-    return pageSelect
+function handleClick(e) {
+    console.log('hello')
 }
 
-function pageSelectHandleChange() {
-    console.log(this.value)
-    if (this.value && this.value !== 'undefined') {
-        currentPage = this.value
-        getAllCharacters(
-            `https://rickandmortyapi.com/api/character/?page=${this.value}`
-        )
-    }
-}
+// function createNav(prev, next, pages) {
+//     nav.innerHTML = ''
+//     if (prev) {
+//         nav.innerHTML += `<button type="button" id="prev-btn" class="absolute left-5" onclick="navBtnHandleClick('${prev}', ${
+//             currentPage - 1
+//         })"><</button>`
+//     }
+//     const pageSelect = createSelectElement(pages)
+//     pageSelect.innerHTML = `
+//     <option value="">--Aller à--</option>`
+//     for (let i = 1; i < pages + 1; i++) {
+//         const option = document.createElement('option')
+//         option.value = i
+//         option.innerText = `page ${i}`
+//         pageSelect.append(option)
+//     }
+//     nav.append(pageSelect)
+//     console.log(pageSelect.onchange)
+//     pageNumber.innerHTML = `${currentPage}/${pages}`
+//     nav.append(pageNumber)
+//     if (next) {
+//         nav.innerHTML += `<button type="button" id="next-btn" class="absolute right-5" onclick="navBtnHandleClick('${next}', ${
+//             currentPage + 1
+//         })">></button>`
+//     }
+// }
+
+// function createSelectElement(pages) {
+//     pageSelect.innerHTML = `
+//     <option value="">--Aller à--</option>`
+//     for (let i = 1; i < pages + 1; i++) {
+//         const option = document.createElement('option')
+//         option.value = i
+//         option.innerText = `page ${i}`
+//         pageSelect.append(option)
+//     }
+//     // pageSelect.onchange = pageSelectHandleChange
+
+//     return pageSelect
+// }
+
+// function pageSelectHandleChange() {
+//     console.log('hello')
+// }
 
 function navBtnHandleClick(nextURL, nextPageNumber) {
     if (nextPageNumber > currentPage) {
@@ -87,6 +110,17 @@ function navBtnHandleClick(nextURL, nextPageNumber) {
     getAllCharacters(nextURL)
 }
 
-// function displayCharacters()
+function displayCharacters(characters) {
+    container.innerHTML = ''
+    const cards = characters.map((character) => {
+        const card = document.createElement('div')
+        card.classList.add('card')
+        card.innerHTML = `
+        <img src="${character.image}" alt="${character.name}" />
+        <h2>${character.name}</h2>`
+        return card
+    })
+    container.append(...cards)
+}
 
 getAllCharacters()
