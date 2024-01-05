f5f825c055a31f48a38957b0e7f10b8e9794fde5
antdol <antoine.dolivet@gmail.com>
2024-01-05 14:48:26 +0100

    fixed everything

--- assets/js/api.js
+++ assets/js/api.js
@@ -4,7 +4,6 @@ const pageNumber = document.createElement('p')
 pageNumber.classList.add('text-white')
 let currentPage = 1
 
-// Récupérer les données de l'api
 async function getAllCharacters(
     url = 'https://rickandmortyapi.com/api/character'
 ) {
@@ -15,91 +14,55 @@ async function getAllCharacters(
         }
         const characters = await response.json()
 
-        // createNav(
-        //     characters.info.prev,
-        //     characters.info.next,
-        //     characters.info.pages
-        // )
-        const prev = characters.info.prev
-        const next = characters.info.next
-        const pages = characters.info.pages
-        nav.innerHTML = ''
-        if (prev) {
-            nav.innerHTML += `<button type="button" id="prev-btn" class="absolute left-5" onclick="navBtnHandleClick('${prev}', ${
-                currentPage - 1
-            })"><</button>`
-        }
-        nav.innerHTML += `
-        <select name="select-page" id="select-page" onchange="handleClick()">
-            <option value="">--Aller à--</option>`
-        const selectPage = document.querySelector('#select-page')
-        for (let i = 1; i < pages + 1; i++) {
-            selectPage.innerHTML += `
-            <option value="${i}">${i}</option>`
-        }
-        pageNumber.innerHTML = `${currentPage}/${pages}`
-        nav.append(pageNumber)
-        if (next) {
-            nav.innerHTML += `<button type="button" id="next-btn" class="absolute right-5" onclick="navBtnHandleClick('${next}', ${
-                currentPage + 1
-            })">></button>`
-        }
+        createNav(
+            characters.info.prev,
+            characters.info.pages,
+            characters.info.next
+        )
 
-        // Affiche les personnage sous forme grille
         displayCharacters(characters.results)
     } catch (error) {
         console.log(error)
     }
 }
 
-function handleClick(e) {
-    console.log('hello')
+function createNav(prev, pages, next) {
+    nav.innerHTML = ''
+    if (prev) {
+        nav.innerHTML += `<button type="button" id="prev-btn" class="absolute left-5" onclick="navBtnHandleClick('${prev}', ${
+            currentPage - 1
+        })"><</button>`
+    }
+    nav.innerHTML += createSelectElement(pages)
+    nav.innerHTML += pageNumber.innerHTML = `${currentPage}/${pages}`
+    nav.append(pageNumber)
+    if (next) {
+        nav.innerHTML += `<button type="button" id="next-btn" class="absolute right-5" onclick="navBtnHandleClick('${next}', ${
+            currentPage + 1
+        })">></button>`
+    }
 }
 
-// function createNav(prev, next, pages) {
-//     nav.innerHTML = ''
-//     if (prev) {
-//         nav.innerHTML += `<button type="button" id="prev-btn" class="absolute left-5" onclick="navBtnHandleClick('${prev}', ${
-//             currentPage - 1
-//         })"><</button>`
-//     }
-//     const pageSelect = createSelectElement(pages)
-//     pageSelect.innerHTML = `
-//     <option value="">--Aller à--</option>`
-//     for (let i = 1; i < pages + 1; i++) {
-//         const option = document.createElement('option')
-//         option.value = i
-//         option.innerText = `page ${i}`
-//         pageSelect.append(option)
-//     }
-//     nav.append(pageSelect)
-//     console.log(pageSelect.onchange)
-//     pageNumber.innerHTML = `${currentPage}/${pages}`
-//     nav.append(pageNumber)
-//     if (next) {
-//         nav.innerHTML += `<button type="button" id="next-btn" class="absolute right-5" onclick="navBtnHandleClick('${next}', ${
-//             currentPage + 1
-//         })">></button>`
-//     }
-// }
+function createSelectElement(pages) {
+    let selectElement = `
+    <select name="select-page" id="select-page" onchange="pageSelectHandleChange()">
+        <option value="">--Aller à--</option>`
+    for (let i = 1; i < pages + 1; i++) {
+        selectElement += `
+        <option value="${i}">${i}</option>`
+    }
+    return selectElement
+}
 
-// function createSelectElement(pages) {
-//     pageSelect.innerHTML = `
-//     <option value="">--Aller à--</option>`
-//     for (let i = 1; i < pages + 1; i++) {
-//         const option = document.createElement('option')
-//         option.value = i
-//         option.innerText = `page ${i}`
-//         pageSelect.append(option)
-//     }
-//     // pageSelect.onchange = pageSelectHandleChange
-
-//     return pageSelect
-// }
-
-// function pageSelectHandleChange() {
-//     console.log('hello')
-// }
+function pageSelectHandleChange() {
+    const selectPage = document.querySelector('#select-page')
+    if (selectPage.value) {
+        currentPage = selectPage.value
+        getAllCharacters(
+            `https://rickandmortyapi.com/api/character/?page=${selectPage.value}`
+        )
+    }
+}
 
 function navBtnHandleClick(nextURL, nextPageNumber) {
     if (nextPageNumber > currentPage) {
